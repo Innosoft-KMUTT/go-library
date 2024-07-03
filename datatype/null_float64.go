917f549019715bd59aaf1e17571c2334b8da51a8
ylnq <wiroon.sri@innosoft.kmutt.ac.th>
2024-07-03 09:34:25 +0000

    add NullFloat64 datatype

---
+++ datatype/null_float64.go
@@ -0,0 +1,69 @@
+package datatype
+
+import (
+	"database/sql"
+	"encoding/json"
+	"strconv"
+)
+
+// NullFloat64 is a custom type that embeds sql.NullFloat64
+type NullFloat64 struct {
+	sql.NullFloat64
+}
+
+// MarshalJSON customizes the JSON marshalling for NullFloat64
+func (nf NullFloat64) MarshalJSON() ([]byte, error) {
+	if !nf.Valid {
+		return json.Marshal(nil)
+	}
+	return json.Marshal(nf.Float64)
+}
+
+// UnmarshalJSON customizes the JSON unmarshalling for NullFloat64
+func (nf *NullFloat64) UnmarshalJSON(data []byte) error {
+	var errString error
+	var errFloat error
+
+	// Unmarshal the data into a float
+	var val *float64
+	if err := json.Unmarshal(data, &val); err != nil {
+		errFloat = err
+	}
+
+	// Unmarshal the data into a string
+	var str string
+	if err := json.Unmarshal(data, &str); err != nil {
+		errString = err
+	}
+
+	if errString != nil && errFloat != nil {
+		if errString != nil {
+			return errString
+		}
+
+		if errFloat != nil {
+			return errFloat
+		}
+	} else if errFloat == nil {
+		nf.Float64 = *val
+		nf.Valid = true
+		return nil
+	}
+
+	// If the string is empty, set the value to nil (invalid)
+	if str == "" {
+		nf.Valid = false
+		return nil
+	}
+
+	// Parse the string to float64
+	value, err := strconv.ParseFloat(str, 64)
+	if err != nil {
+		return err
+	}
+
+	// Set the value and mark it as valid
+	nf.Float64 = value
+	nf.Valid = true
+	return nil
+}
---
+++ datatype/null_float64_test.go
@@ -0,0 +1,62 @@
+package datatype
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type TestNullFloat64Type struct {
+	Value NullFloat64 `json:"value"`
+}
+
+func TestNullFloat64(t *testing.T) {
+	fmt.Println("TestNullFloat64")
+
+	var data TestNullFloat64Type
+	var s string
+
+	s = string(`{}`)
+	data = TestNullFloat64Type{}
+	json.Unmarshal([]byte(s), &data)
+	fmt.Printf("s: %s\n", s)
+	fmt.Printf("value: %v\n", data.Value)
+	assert.Equal(t, data.Value.Valid, false, "value Valid should be false")
+
+	s = string(`{"value":0}`)
+	data = TestNullFloat64Type{}
+	json.Unmarshal([]byte(s), &data)
+	fmt.Printf("s: %s\n", s)
+	fmt.Printf("value: %v\n", data.Value)
+	assert.Equal(t, data.Value.Float64, 0, "value Float64 should be 0")
+
+	s = string(`{"value":1.1}`)
+	data = TestNullFloat64Type{}
+	json.Unmarshal([]byte(s), &data)
+	fmt.Printf("s: %s\n", s)
+	fmt.Printf("value: %v\n", data.Value)
+	assert.Equal(t, data.Value.Float64, 1.1, "value Float64 should be 1.1")
+
+	s = string(`{"value":""}`)
+	data = TestNullFloat64Type{}
+	json.Unmarshal([]byte(s), &data)
+	fmt.Printf("s: %s\n", s)
+	fmt.Printf("value: %v\n", data.Value)
+	assert.Equal(t, data.Value.Valid, false, "value Valid should be false")
+
+	s = string(`{"value":"0"}`)
+	data = TestNullFloat64Type{}
+	json.Unmarshal([]byte(s), &data)
+	fmt.Printf("s: %s\n", s)
+	fmt.Printf("value: %v\n", data.Value)
+	assert.Equal(t, data.Value.Float64, 0.0, "value Float64 should be 0")
+
+	s = string(`{"value":"1.1"}`)
+	data = TestNullFloat64Type{}
+	json.Unmarshal([]byte(s), &data)
+	fmt.Printf("s: %s\n", s)
+	fmt.Printf("value: %v\n", data.Value)
+	assert.Equal(t, data.Value.Float64, 1.1, "value Float64 should be 1.1")
+}
